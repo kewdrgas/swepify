bc9ec89c49b3f1718668b545b5c7aaa94aed1e14
KEWIN Drgas <kewindrgas@Laptop-KEWIN.local>
2022-01-09 02:39:52 +0100

    backend

---
+++ database/migrations/2022_01_09_012617_fix_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+                    Schema::table('users', function (Blueprint $table) {
+                 $table->rememberToken()->after('password');
+             });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ resources/views/dashboard/sections/header.blade.php
@@ -0,0 +1,36 @@
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container row-fluid">
+            <a class="navbar-brand" href="{{route('home')}}">Logo</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-between">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{route('home')}}">Swepify </a>
+                    </li>
+                </ul>
+
+                <div class="menu--buttons d-flex">
+                    <a href="{{route('offers')}}" class="nav-link">Przeglądaj oferty</a>
+                    <a href="{{route('locations')}}" class="nav-link">Lokalizacje</a>
+                    <a href="{{route('locations')}}" class="nav-link">Moje zamówienia</a>
+                    <a href="{{route('login')}}" class="button button__whiteorange"
+                        >Profil</a
+                    >
+                    <a href="{{route('login')}}" class="button button__orange"
+                        >Wyloguj się</a
+                    >
+                </div>
+            </div>
+        </div>
+    </nav>
